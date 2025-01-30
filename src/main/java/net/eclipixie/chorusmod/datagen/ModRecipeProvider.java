a6dfe7b6fba86e9f99415260da9e1656936a4114
Eclipixie <85773134+Eclipixie@users.noreply.github.com>
2025-01-30 20:43:28 +1100

    Update ModRecipeProvider.java

--- src/main/java/net/eclipixie/chorusmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/eclipixie/chorusmod/datagen/ModRecipeProvider.java
@@ -1,19 +1,16 @@
 package net.eclipixie.chorusmod.datagen;
 
-import net.eclipixie.chorusmod.ChorusMod;
 import net.eclipixie.chorusmod.block.ModBlocks;
 import net.eclipixie.chorusmod.item.ModItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -23,44 +20,60 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     }
 
     @Override
-    protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
-        
+    protected void buildRecipes(@NotNull Consumer<FinishedRecipe> consumer) {
+        oreBlasting(
+                consumer,
+                List.of(Items.ENDER_PEARL),
+                RecipeCategory.MISC,
+                ModItems.ENDER_SHARD.get(),
+                0.75f,
+                100,
+                "ender_shard");
+
+        ShapelessRecipeBuilder.shapeless(
+                    RecipeCategory.BUILDING_BLOCKS,
+                    ModBlocks.VOIDGLASS.get(), 1)
+                .requires(ModItems.ENDER_SHARD.get(), 4)
+                .requires(Items.GLASS)
+                .unlockedBy("has_ender_shard", has(ModItems.ENDER_SHARD.get()))
+                .save(consumer, getItemName(ModBlocks.VOIDGLASS.get()) + "_mono");
+
+        ShapelessRecipeBuilder.shapeless(
+                        RecipeCategory.BUILDING_BLOCKS,
+                        ModBlocks.VOIDGLASS.get(), 2)
+                .requires(ModItems.ENDER_SHARD.get(), 7)
+                .requires(Items.GLASS, 2)
+                .unlockedBy("has_ender_shard", has(ModItems.ENDER_SHARD.get()))
+                .save(consumer, getItemName(ModBlocks.VOIDGLASS.get()) + "_duo");
+
+        // smithing recipes go template, subject, material
+
+        // voidburst spyglass
+        customSmithingRecipe(
+                ModBlocks.VOIDGLASS.get(),
+                Items.SPYGLASS,
+                Items.TWISTING_VINES,
+                RecipeCategory.TOOLS,
+                ModItems.VOIDBURST_SPYGLASS.get(),
+                consumer);
+
+        // soundsight spyglass
+//        customSmithingRecipe(
+//                Ingredient.of(ModItems.RESONATOR.get()),
+//                Ingredient.of(Items.SPYGLASS),
+//                Ingredient.of(Items.TWISTING_VINES),
+//                RecipeCategory.TOOLS,
+//                ModItems.VOIDBURST_SPYGLASS.get(),
+//                consumer);
     }
 
-    protected static void nineBlock(Item smaller, Block larger, Consumer<FinishedRecipe> consumer) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, larger)
-                .pattern("sss")
-                .pattern("sss")
-                .pattern("sss")
-                .define('s', smaller)
-                .unlockedBy(getHasName(smaller), has(smaller))
-                .save(consumer);
-
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, smaller, 9)
-                .requires(larger)
-                .unlockedBy(getHasName(larger), has(larger))
-                .save(consumer);
-    }
-
-    protected static void oreSmelting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTIme, String pGroup) {
-        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.SMELTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTIme, pGroup, "_from_smelting");
-    }
-
-    protected static void oreBlasting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
-        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
-    }
-
-    protected static void oreCooking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, RecipeSerializer<? extends AbstractCookingRecipe> pCookingSerializer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
-        Iterator var9 = pIngredients.iterator();
-
-        while(var9.hasNext()) {
-            ItemLike itemlike = (ItemLike)var9.next();
-            SimpleCookingRecipeBuilder
-                    .generic(Ingredient.of(new ItemLike[]{itemlike}), pCategory, pResult, pExperience, pCookingTime, pCookingSerializer)
-                    .group(pGroup)
-                    .unlockedBy(getHasName(itemlike), has(itemlike))
-                    .save(pFinishedRecipeConsumer, ChorusMod.MOD_ID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
-        }
-
+    protected void customSmithingRecipe(ItemLike template, ItemLike base, ItemLike addition, RecipeCategory category, Item result, Consumer<FinishedRecipe> consumer) {
+        SmithingTransformRecipeBuilder.smithing(
+                        Ingredient.of(template),
+                        Ingredient.of(base),
+                        Ingredient.of(addition),
+                        category, result)
+                .unlocks("has_"+getItemName(template), has(template))
+                .save(consumer, getItemName(result) + "_smithing");
     }
 }
