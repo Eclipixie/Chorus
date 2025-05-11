58d96f089addb4c4010247dd95357dcad21452d3
eclipixie <85773134+Eclipixie@users.noreply.github.com>
2025-05-11 20:28:55 +1000

    Update SculkExoskeletonArmor.java
    unbreakable

--- src/main/java/net/eclipixie/chorusmod/item/custom/armor/SculkExoskeletonArmor.java
+++ src/main/java/net/eclipixie/chorusmod/item/custom/armor/SculkExoskeletonArmor.java
@@ -1,19 +1,11 @@
 package net.eclipixie.chorusmod.item.custom.armor;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.eclipixie.chorusmod.ChorusMod;
 import net.eclipixie.chorusmod.entity.client.model.SculkExoskeletonModel3;
 import net.eclipixie.chorusmod.item.ModArmorMaterials;
 import net.eclipixie.chorusmod.item.custom.ArmorSetCore;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.Model;
 import net.minecraft.client.renderer.LightTexture;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
@@ -21,6 +13,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import org.jetbrains.annotations.NotNull;
@@ -38,6 +32,9 @@ public class SculkExoskeletonArmor extends ArmorItem {
             player.setInvisible(ArmorSetCore.checkSet(ModArmorMaterials.SCULK_EXOSKELETON, player));
         }
     }
+
+    @Override
+    public boolean canBeDepleted() { return false; }
 
     @Override
     public void initializeClient(java.util.function.Consumer<net.minecraftforge.client.extensions.common.IClientItemExtensions> consumer) {
